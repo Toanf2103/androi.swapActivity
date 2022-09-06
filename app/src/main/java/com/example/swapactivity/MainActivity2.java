b9efb47e115251f5140a67a0fbf1568be1769ae0
Toanf2103 <nobitakute002@gmail.com>
2022-09-06 08:13:11 +0700

    xong

--- app/src/main/java/com/example/swapactivity/MainActivity2.java
+++ app/src/main/java/com/example/swapactivity/MainActivity2.java
@@ -7,17 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnSwap;
+public class MainActivity2 extends AppCompatActivity {
+    Button  btnSwap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         btnSwap = (Button) findViewById(R.id.btnSwapView);
         btnSwap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent( MainActivity.this,MainActivity2.class);
+                Intent i = new Intent( MainActivity2.this,MainActivity.class);
                 startActivity(i);
             }
         });
